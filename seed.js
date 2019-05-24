70dd6e48244839827943668a12db95fb5c743e43
Beno Suriano <bjsuriano@gmail.com>
2019-05-24 08:24:26 -0600

    Sync with force true.

---
+++ seed.js
@@ -0,0 +1,45 @@
+/**
+ * const promiseForDog = Dog.findAll()
+ *
+ * promiseForDog.then((dog) => {
+ *  console.log('Got a dog over here:' , dog)
+ * })
+ *
+ * Promise.then(success, err)
+ *
+ * Promise Chaining
+ *
+ * The trick: every call to .then returns a promise
+ *
+ * const p1 = Dog.findById(1)
+ * const p2 = p1.then(dog => {
+ *  return dog.update()
+ * })
+ *
+ * p2.then(result => {
+ *  console.log(result)
+ * })
+ *
+ * or...
+ *
+ * Dog.findById(1)
+ *  .then(dog => {
+ *  return dog.update()
+ * })
+ *  .then(result => {
+ *  console.log(result)
+ * })
+ *  .catch(err => console.error(err))
+ */
+
+const db = require('./models')
+
+db.sync({force: true})
+  .then(() => {
+    db.close()
+  })
+  .catch(() => {
+    console.error('There has been some kind of error. Please try again later.')
+    db.close()
+  })
+
