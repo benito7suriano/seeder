cc2e65ddd7eb71ebe884fe04e399a154a3b72385
Beno Suriano <bjsuriano@gmail.com>
2019-05-24 08:29:06 -0600

    Define models.

--- models.js
+++ models.js
@@ -1,4 +1,19 @@
 const Sequelize = require('Sequelize')
 const db = new Sequelize('postgres://localhost:5432/plantr')
 
+const Gardener = db.define('gardeners', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+})
+
+const Plot = db.define('plots', {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+})
+
+const Vegetable = db.define('vegetables', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING
+})
+
 module.exports = db
