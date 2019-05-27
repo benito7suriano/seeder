0fe3589ed13f88b16570eb5496cceefb715f6bb4
Beno Suriano <bjsuriano@gmail.com>
2019-05-27 08:16:58 -0600

    refactored to async await.

--- seed.js
+++ seed.js
@@ -31,34 +31,73 @@
  * })
  *  .catch(err => console.error(err))
  */
+
+// USING PROMISES
+
+// const { db, Gardener, Plot, Vegetable } = require('./models')
+
+// let brocolliG
+
+// db.sync({ force: true })
+//   .then(() => {
+//     console.log('Db sync\'d')
+//     return Promise.all([
+//       Vegetable.create({name: 'brocolli'}),
+//       Vegetable.create({name: 'cauliflor'}),
+//       Vegetable.create({name: 'lettuce'})
+//     ])
+//   })
+//   .then(([brocolli, cauliflor, lettuce]) => {
+//     brocolliG = brocolli
+
+//     return Gardener.create({name: 'Beno', favoriteVegetableId: brocolli.id})
+//   })
+//   .then((beno) => {
+//     return Plot.create({
+//       gardenerId: beno.id
+//     })
+//   })
+//   .then((plot) => {
+//     return plot.addVegetable(brocolliG)
+//     db.close()
+//   })
+//   .catch(() => {
+//     console.error('There has been some kind of error. Please try again later.')
+//     db.close()
+//   })
+
+
+// USING ASYNC AWAIT
+
 const { db, Gardener, Plot, Vegetable } = require('./models')
 
-let brocolliG
+const seed = async() => {
+  await db.sync({ force: true })
 
-db.sync({ force: true })
-  .then(() => {
-    console.log('Db sync\'d')
-    return Promise.all([
-      Vegetable.create({name: 'brocolli'}),
-      Vegetable.create({name: 'cauliflor'}),
-      Vegetable.create({name: 'lettuce'})
-    ])
-  })
-  .then(([brocolli, cauliflor, lettuce]) => {
-    brocolliG = brocolli
+  const [brocolli, cauliflor, lettuce] = await Promise.all([
+    Vegetable.create({ name: 'brocolli' }),
+    Vegetable.create({ name: 'cauliflor' }),
+    Vegetable.create({ name: 'lettuce' })
+  ])
 
-    return Gardener.create({name: 'Beno', favoriteVegetableId: brocolli.id})
+  const gardener = await Gardener.create({
+    name: 'Beno',
+    favoriteVegetableId: brocolli.id
   })
-  .then((beno) => {
-    return Plot.create({
-      gardenerId: beno.id
-    })
+
+  const plot = await Plot.create({
+    gardenerId: gardener.id
   })
-  .then((plot) => {
-    return plot.addVegetable(brocolliG)
-    db.close()
-  })
-  .catch(() => {
-    console.error('There has been some kind of error. Please try again later.')
-    db.close()
-  })
+
+  await plot.addVegetable(brocolli)
+
+  console.log('Db synced and closing...')
+
+  db.close()
+}
+
+seed()
+.catch(() => {
+  console.error('There has been some kind of error. Please try again later.')
+  db.close()
+})
